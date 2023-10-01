0b3268780f784598a9688522b3e089af0f313cb5
jimmy90s <jimmy90s@outlook.com>
2023-09-30 22:10:14 -0700

    Created a PostPreview componet to style each post in out return.

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,57 +1,209 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
-import { Post } from "../API";
-import { Grid } from "@mui/material";
+import { ButtonBase, Grid, IconButton, Paper, Typography } from "@mui/material";
+import React, { ReactElement, useState, useEffect } from "react";
+import {
+  // CreateVoteInput,
+  // CreateVoteMutation,
+  Post,
+  // UpdateVoteInput,
+  // UpdateVoteMutation,
+  // UpdateVoteMutationVariables,
+} from "../API";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import Image from "next/image";
+import { useRouter } from "next/router";
+// import formatDatePosted from "../lib/formatDatePosted";
+import { API, Auth, Storage } from "aws-amplify";
+// import { createVote, updateVote } from "../graphql/mutations";
+import { GRAPHQL_AUTH_MODE } from "@aws-amplify/api";
+import { useUser } from "../context/AuthContext";
 
 interface Props {
   post: Post;
 }
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
 
-export default function PostPreview({ post }: Props) {
+export default function PostPreview({ post }: Props): ReactElement {
+  const router = useRouter();
+  const { user } = useUser();
+  const [postImage, setPostImage] = useState<string | undefined>(undefined);
+  const [existingVote, setExistingVote] = useState<string | undefined>(
+    undefined
+  );
+  const [existingVoteId, setExistingVoteId] = useState<string | undefined>(
+    undefined
+  );
+  // const [upvotes, setUpvotes] = useState<number>(
+  //   post.votes.items
+  //     ? post.votes.items.filter((v) => v.vote === "upvote").length
+  //     : 0
+  // );
+
+  // const [downvotes, setDownvotes] = useState<number>(
+  //   post.votes.items
+  //     ? post.votes.items.filter((v) => v.vote === "downvote").length
+  //     : 0
+  // );
+
+  // useEffect(() => {
+  //   if (user) {
+  //     const tryFindVote = post.votes.items?.find(
+  //       (v) => v.owner === user.getUsername()
+  //     );
+
+  //     if (tryFindVote) {
+  //       setExistingVote(tryFindVote.vote);
+  //       setExistingVoteId(tryFindVote.id);
+  //     }
+  //   }
+  // }, [user]);
+
+  useEffect(() => {
+    async function getImageFromStorage() {
+      try {
+        const signedURL = await Storage.get(post.title); // get key from Storage.list
+        console.log("Found Image:", signedURL);
+        // @ts-ignore
+        setPostImage(signedURL);
+      } catch (error) {
+        console.log("No image found.");
+      }
+    }
+
+    getImageFromStorage();
+  }, []);
+
+  // const addVote = async (voteType: string) => {
+  //   if (existingVote && existingVote != voteType) {
+  //     const updateVoteInput: UpdateVoteInput = {
+  //       id: existingVoteId,
+  //       vote: voteType,
+  //       postID: post.id,
+  //     };
+
+  //     const updateThisVote = (await API.graphql({
+  //       query: updateVote,
+  //       variables: { input: updateVoteInput },
+  //       authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
+  //     })) as { data: UpdateVoteMutation };
+  //     // if they're changing their vote...
+  //     // updateVote rather than create vote.
+
+  //     if (voteType === "upvote") {
+  //       setUpvotes(upvotes + 1);
+  //       setDownvotes(downvotes - 1);
+  //     }
+
+  //     if (voteType === "downvote") {
+  //       setUpvotes(upvotes - 1);
+  //       setDownvotes(downvotes + 1);
+  //     }
+  //     setExistingVote(voteType);
+  //     setExistingVoteId(updateThisVote.data.updateVote.id);
+  //     console.log("Updated vote:", updateThisVote);
+  //   }
+
+  //   if (!existingVote) {
+  //     const createNewVoteInput: CreateVoteInput = {
+  //       vote: voteType,
+  //       postID: post.id,
+  //     };
+
+  //     const createNewVote = (await API.graphql({
+  //       query: createVote,
+  //       variables: { input: createNewVoteInput },
+  //       authMode: GRAPHQL_AUTH_MODE.AMAZON_COGNITO_USER_POOLS,
+  //     })) as { data: CreateVoteMutation };
+
+  //     if (createNewVote.data.createVote.vote === "downvote") {
+  //       setDownvotes(downvotes + 1);
+  //     }
+  //     if (createNewVote.data.createVote.vote === "upvote") {
+  //       setUpvotes(upvotes + 1);
+  //     }
+  //     setExistingVote(voteType);
+  //     setExistingVoteId(createNewVote.data.createVote.id);
+  //     console.log("Created vote:", createNewVote);
+  //   }
+  // };
+
+  // console.log(post);
+  // console.log("Upvotes:", upvotes);
+  // console.log("Downvotes:", downvotes);
+
   return (
-    <Grid
-      container
-      style={{ marginTop: 16 }}
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-      direction="column"
-      alignContent="center"
-      justifyContent="center"
-    >
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography
-            sx={{ fontSize: 14 }}
-            color="text.secondary"
-            gutterBottom
-          ></Typography>
-          <Typography variant="h5" component="div">
-            {post.title}
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary"></Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    </Grid>
+    <Paper elevation={3}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        alignContent="center"
+        justifyContent="center"
+        direction="row"
+        wrap="nowrap"
+        style={{ padding: 24, marginTop: 24 }}
+      >
+        {/* Upvote / votes / downvote */}
+        <Grid item style={{ maxWidth: 128 }}>
+          <Grid container direction="column" alignItems="center">
+            <Grid item>
+              <IconButton color="inherit">
+                <ArrowUpwardIcon />
+              </IconButton>
+            </Grid>
+            <Grid item>
+              <Grid container alignItems="center" direction="column">
+                <Grid item>
+                  <Typography variant="h6"></Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="body2">votes</Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item>
+              <IconButton color="inherit">
+                <ArrowDownwardIcon />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        {/* Content Preview */}
+        <Grid item>
+          <ButtonBase onClick={() => router.push(`/post/${post.id}`)}>
+            <Grid container direction="column" alignItems="flex-start">
+              <Grid item>
+                <Typography variant="body1">
+                  Posted by <b>{post.owner}</b>{" "}
+                  {/* {formatDatePosted(post.createdAt)} hours ago */}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="h2">{post.title}</Typography>
+              </Grid>
+              <Grid
+                item
+                style={{
+                  maxHeight: 32,
+                  overflowY: "hidden",
+                  overflowX: "hidden",
+                }}
+              >
+                {/* <Typography variant="body1">{post.contents}</Typography>
+              </Grid>
+              {post.image && postImage && (
+                <Grid item>
+                  <Image
+                  src={postImage}
+                  height={540}
+                  width={980}
+                  layout="intrinsic" alt={""}                  />
+                </Grid>
+              )} */}
+              </Grid>
+            </Grid>
+          </ButtonBase>
+        </Grid>
+      </Grid>
+    </Paper>
   );
 }
